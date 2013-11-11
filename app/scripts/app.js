e3f1201a5a707e35fb5bd032bb66cc859c4e81f6
Francis Brito <fr.br94@gmail.com>
2013-11-11 08:43:00 -0400

    Added application files

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+function TaskListCtrl ($scope) {
+    $scope.tasks = [
+        {
+            description: 'Finish Angular.js challenge.',
+            done: false
+        },
+        {
+            description: 'Visit cardiologist.',
+            done: false
+        },
+        {
+            description: 'Change the world.',
+            done: false
+        },
+        {
+            description: 'Lorem',
+            done: true
+        },
+        {
+            description: 'Ipsum',
+            done: true
+        }
+    ];
+
+    $scope.newTask = '';
+
+    $scope.addTask = function (t) {
+        var newTask = {
+            description: $scope.newTask,
+            done: false
+        };
+
+        tasks.push(newTask);
+    }
+}
